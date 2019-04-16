8fd5dde8c747b423cd3117ee7cd5411b13120995
Milos Petrovic <milos.brugi@gmail.com>
2019-04-16 11:28:35 -0400

    Adding Dashboard page with base layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import AmbientMeasurement from './components/ambient/ambientMeasurement';
 import './App.css';
+import DashboardPage from './components/dashboard/DashboardPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <AmbientMeasurement/>
+        <DashboardPage/>
       </div>
     );
   }
