9a769efeb0d694b9c40129564e7f2f97d58b15ab
Milos Petrovic <milos.brugi@gmail.com>
2019-04-23 14:57:10 -0400

    Fixing card link.

--- src/components/item/Item.tsx
+++ src/components/item/Item.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Col, Card } from 'antd';
+import { Link } from 'react-router-dom';
 
 const Item: React.FunctionComponent<{}> = () => {
     return (
@@ -7,7 +8,7 @@ const Item: React.FunctionComponent<{}> = () => {
             <Col xs={24} sm={12} lg={6}>
                 <Card title="This is title"
                     bordered={false}
-                    extra={<a href="/history">Details</a>}>
+                    extra={<Link to="/history">Details</Link>}>
                     <p>Starting weight:</p>
                     <p>1200g</p>
                     <p>Target weight:</p>
