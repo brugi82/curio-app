33afda2c6e36ea43160c015760f4c2b52e2ae649
Milos Petrovic <milos.brugi@gmail.com>
2019-04-24 14:10:39 -0400

    Adding more info for Item.

--- src/components/item/Item.tsx
+++ src/components/item/Item.tsx
@@ -9,12 +9,22 @@ const Item: React.FunctionComponent<{}> = () => {
                 <Card title="This is title"
                     bordered={false}
                     extra={<Link to="/history">Details</Link>}>
-                    <p>Starting weight:</p>
-                    <p>1200g</p>
-                    <p>Target weight:</p>
-                    <p>900g</p>
-                    <p>Current weight:</p>
-                    <p>1100g</p>
+                    <div style={{ width: '50%', float: 'left' }}>
+                        <p className="item-value-label"><i>Starting weight</i></p>
+                        <p>1200g</p>
+                        <p className="item-value-label"><i>Target weight</i></p>
+                        <p>900g</p>
+                        <p className="item-value-label"><i>Current weight</i></p>
+                        <p>1100g</p>
+                    </div>
+                    <div style={{ width: '50%', float: 'right' }}>
+                        <p className="item-value-label"><i>Started on</i></p>
+                        <p>April 3rd 2019</p>
+                        <p className="item-value-label"><i>Projected finish</i></p>
+                        <p>May 10th 2019</p>
+                        <p className="item-value-label"><i>Last measurement</i></p>
+                        <p>April 22nd 2019</p>
+                    </div>
                 </Card>
             </Col>
         </div>
